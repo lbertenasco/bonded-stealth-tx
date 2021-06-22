ba80f4aec7d065f7c9ba6813adb0c54b26723469
Luciano Bertenasco <lbertenasco@gmail.com>
2021-06-22 17:47:51 -0300

    feat: add deploy script (#6)
    * feat: add deploy script
    
    * chore: updates lock files
    
    * feat: removed governor argument, add relayer deploy script
    
    * feat: add coinbase payment on report and work (#10)
    
    * fix: e2e test relayer deploy arguments
    
    * fix: package-lock gh actions error
    
    * fix: force clean npm i
    
    * feat: remove web3 duplicated zeroaddress
    
    * fix: tests are failing, not sure why
    
    * feat: remove unused watcher script
    
    * fix: using packages from main branch

--- contracts/StealthRelayer.sol
+++ contracts/StealthRelayer.sol
@@ -21,7 +21,7 @@ contract StealthRelayer is Governable, CollectableDust, StealthTx, IStealthRelay
   bool public override forceBlockProtection;
   address public override caller;
 
-  constructor(address _governor, address _stealthVault) Governable(_governor) StealthTx(_stealthVault) {}
+  constructor(address _stealthVault) Governable(msg.sender) StealthTx(_stealthVault) {}
 
   modifier onlyValidJob(address _job) {
     require(_jobs.contains(_job), 'SR: invalid job');
