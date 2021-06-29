155db95b56f93e4662f390d4bbdde2dfe134be67
Luciano Bertenasco <lbertenasco@gmail.com>
2021-06-29 19:54:38 -0300

    feat: add goerli flashbots scripts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -1,9 +1,14 @@
 export const stealthVault = {
   mainnet: '',
-  goerli: '',
+  goerli: '0x093a60a183245b9ce41fBe5Bd0ef6b1A0AA52A65',
 };
 
 export const stealthRelayer = {
   mainnet: '',
-  goerli: '',
+  goerli: '0x6ABEF8eF9dF993c5a8f32484E0ae248281227C83',
+};
+
+export const stealthERC20 = {
+  mainnet: '',
+  goerli: '0x4dC5C8955EC833A8C5D865c26fa85649522D033E',
 };
